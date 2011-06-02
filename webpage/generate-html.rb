2416505cc6c130086394d3c0a6bf6dd22fb7eebf
Darshan-Josiah Barber <darshan@darshancomputing.com>
2011-06-02 22:21:15 +0000

    [Plugin page]: A tiny bit of work.
    git-svn-id: file:///home/darshan/svn-repo-clone-of-bi-on-google-code/icon-plugins@397 fb11ccbe-488a-4026-b0b0-a5194d29edc0

--- webpage/generate-html.rb
+++ webpage/generate-html.rb
@@ -13,7 +13,7 @@ Dir.glob('../plugin-*').each do |plugin_dir|
 
   system "cp #{plugin_dir}/bin/*-release.apk #{apklocation}"
   system "cp #{plugin_dir}/res/drawable-hdpi/default050.png #{pnglocation}"
-  if not (system "cp #{plugin_dir}/res/drawable-hdpi/charging050.png #{chglocation}")
+  if not (system "cp #{plugin_dir}/res/drawable-hdpi/charging050.png #{chglocation} &>/dev/null")
     chglocation = nil
   end
 
@@ -30,17 +30,20 @@ end
 page = PluginPage.new
 page.addln '<ul>'
 
-page.addln '<table><tr><th>Name</th><th>Normal Icon</th><th>Charging Icon</th><th>Link</th></tr>'
+page.addln '<table><tr><th>Name</th><th colspan="2">Normal Icon</th><th colspan="2">Charging Icon</th><th>Link</th></tr>'
 plugins.each do |plugin|
   page.add '<tr>'
   page.add "<td>#{plugin[:name]}</td>"
   page.add "<td style=\"background-color:black\"><img src=\"#{plugin[:pnglocation]}\"></td>"
+  page.add "<td style=\"background-image:url('./images/pre-gingerbread-bg.png')\"><img src=\"#{plugin[:pnglocation]}\"></td>"
   if plugin[:chglocation].nil?
-    page.add "<td>(Same)</td>"
+    page.add '<td colspan="2">(Same)</td>'
   else
     page.add "<td style=\"background-color:black\"><img src=\"#{plugin[:chglocation]}\"></td>"
+    page.add "<td style=\"background-image:url('./images/pre-gingerbread-bg.png')\"><img src=\"#{plugin[:chglocation]}\"></td>"
   end
-  page.add "<td><a href=\"#{plugin[:apklocation]}\">#{plugin[:apklocation].split('/').last}</a></td>"
+  #page.add "<td><a href=\"#{plugin[:apklocation]}\">#{plugin[:apklocation].split('/').last}</a></td>"
+  page.add "<td><a href=\"#{plugin[:apklocation]}\">Install</a></td>"
   page.addln "</tr>"
 end
 
