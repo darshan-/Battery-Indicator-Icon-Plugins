9903ddda723079e9bea563b4512c5a2a102a8cc1
Darshan-Josiah Barber <darshan@darshancomputing.com>
2011-10-12 20:01:33 -0700

    Updated create_plugin.sh for git (simpler!)

--- create_plugin.sh
+++ create_plugin.sh
@@ -14,30 +14,8 @@ cp -ar skeleton-plugin-dir $plugin_dir
 
 cd $plugin_dir
 
-rm -rf .svn
-rm -rf */.svn
-rm -rf */*/.svn
-rm -rf */*/*/.svn
-rm -rf */*/*/*/.svn
-rm -rf */*/*/*/*/.svn
-rm -rf */*/*/*/*/*/.svn
-rm -rf */*/*/*/*/*/*/.svn
-rm -rf */*/*/*/*/*/*/*/.svn
-
 sed -i -e s/PluginName/$plugin_name/ AndroidManifest.xml
 sed -i -e s/PluginName/$plugin_name/ src/com/darshancomputing/BatteryIndicatorPro/IconPlugin/PluginService.java
 sed -i -e "s/PluginName/$plugin_full_name/" res/values/strings.xml
 
-cd ..
-svn add $plugin_dir
-cd $plugin_dir
-
-svn propset svn:ignore "bin
-gen
-libs" .
-
-svn propset svn:ignore "numbers-hdpi
-numbers-mdpi" image-playground
-
-svn propset svn:ignore "drawable-hdpi
-drawable" res
+git add .
